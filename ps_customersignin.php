edfc10f8bc5b46e4fbff8cca8c8eee20099771ee
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-01 17:40:08 +0100

    // Version updated to v2.0.1

--- ps_customersignin.php
+++ ps_customersignin.php
@@ -38,7 +38,7 @@ class Ps_CustomerSignIn extends Module implements WidgetInterface
     {
         $this->name = 'ps_customersignin';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->need_instance = 0;
 
         parent::__construct();
