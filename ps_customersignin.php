99e436768a49924673102fba91d4fe9c7927780a
Progi1984 <franck.lefevre@prestashop.com>
2020-09-11 16:28:27 +0200

    Bump version to 2.0.3

--- ps_customersignin.php
+++ ps_customersignin.php
@@ -38,7 +38,7 @@ class Ps_CustomerSignIn extends Module implements WidgetInterface
     {
         $this->name = 'ps_customersignin';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.2';
+        $this->version = '2.0.3';
         $this->need_instance = 0;
 
         parent::__construct();
